2f35f89b982b9f52c86176a567dd3d3ed90e82e5
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-20 09:23:28 -0400

    Users are able to see comments on different pizzas and can reply to comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,3 @@
-const { json } = require("express");
 const { Comment, Pizza } = require("../models");
 
 const commentController = {
@@ -20,6 +19,33 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch((err) => res.json(err));
+  },
+
+  // Add replies to comments
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => res.json(err));
   },
 
